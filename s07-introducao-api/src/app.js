0421b72703d4dc213ad98c6a6c7da586a36b8eab
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-10-01 21:16:59 -0300

    orixas-server created

--- s07-introducao-api/src/app.js
+++ s07-introducao-api/src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+const index = require('./routes/index');
+const orixas = require('./routes/orixasRoute')
+
 app.use((req, res, next) => {
     console.log('Nova requisição realizada');
     next();
 });
+
+app.use('/', index);
+app.use('/orixas', orixas);
 
 module.exports = app;
