dce3d92e89d15017d90d18f898e99bf31f82cef4
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-10-15 22:47:50 -0300

    add employee age and syntax change to automatic id

--- s09-api-post-delete/src/controller/livrosController.js
+++ s09-api-post-delete/src/controller/livrosController.js
@@ -1,53 +1,66 @@
-const books = require('../models/livros.json');
-const fs = require('fs');
+const books = require("../models/livros.json");
+const fs = require("fs");
 
 const getAllBooks = (req, res) => {
-    res.status(200).send(books);
+  res.status(200).send(books);
 };
 
 const getBookById = (req, res) => {
-    const id = req.params.id;
-    res.status(200).send(books.find((book) => book.id == id))
+  const id = req.params.id;
+  res.status(200).send(books.find((book) => book.id == id));
 };
 
 const getBooksStock = (req, res) => {
-    const booksStock = books.filter((booksAvaiable) => booksAvaiable.estoque == true);
-    res.status(200).send(booksStock);
+  const booksStock = books.filter(
+    (booksAvaiable) => booksAvaiable.estoque == true
+  );
+  res.status(200).send(booksStock);
 };
-
 
 const postBook = (req, res) => {
-    const { id, titulo, autora, genero, estoque, editora } = req.body;
-    books.push({ id, titulo, autora, genero, estoque, editora });
+  const id = books[books.length - 1].id + 1;
+  const book = req.body;
+  const newBook = { id, ...book };
+  books.push(newBook);
 
-    fs.writeFile('./src/models/livros.json', JSON.stringify(books), 'utf8', err => {
-        if(err) {
-            return res.status(424).send({ message: err });
-        }
-        console.log('Arquivo atualizado com sucesso!');
-    });
-    res.status(201).send(books);
+  fs.writeFile(
+    "./src/models/livros.json",
+    JSON.stringify(books),
+    "utf8",
+    (err) => {
+      if (err) {
+        return res.status(424).send({ message: err });
+      }
+      console.log("Arquivo atualizado com sucesso!");
+    }
+  );
+  res.status(201).send(books);
 };
 
-const deleteBook = (req, res)  => {
-    const id = req.params.id;
-    const filteredBook = books.find((foundBook) => foundBook.id == id);
-    const index = books.indexOf(filteredBook);
-    books.splice(index, 1);
+const deleteBook = (req, res) => {
+  const id = req.params.id;
+  const filteredBook = books.find((foundBook) => foundBook.id == id);
+  const index = books.indexOf(filteredBook);
+  books.splice(index, 1);
 
-    fs.writeFile('./src/models/livros.json', JSON.stringify(books), 'utf8', err => {
-        if(err) {
-            return res.status(424).send({ message: err });
-        };
-        console.log('Arquivo deletado com sucesso!');
-    });
-    res.status(201).send(books);
+  fs.writeFile(
+    "./src/models/livros.json",
+    JSON.stringify(books),
+    "utf8",
+    (err) => {
+      if (err) {
+        return res.status(424).send({ message: err });
+      }
+      console.log("Arquivo deletado com sucesso!");
+    }
+  );
+  res.status(201).send(books);
 };
 
 module.exports = {
-    getAllBooks,
-    getBookById,
-    getBooksStock,
-    postBook,
-    deleteBook,
+  getAllBooks,
+  getBookById,
+  getBooksStock,
+  postBook,
+  deleteBook,
 };
--- s09-api-post-delete/src/routes/funcionariosRoute.js
+++ s09-api-post-delete/src/routes/funcionariosRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controller/funcionariosController');
 
 router.get('/', controller.getAllEmployees);
+router.get('/idade/:id', controller.getEmployeeAge);
 router.get('/lista-nomes', controller.getEmployeeList);
 router.get('/:id', controller.getEmployeeById);
 router.post('/', controller.postEmployee);
