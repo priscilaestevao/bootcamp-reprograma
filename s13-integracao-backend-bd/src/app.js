f55b6030dc6d0092f62a497fe9896ca2bb6967a4
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-04 23:49:45 -0300

    creation connection string with the mongo

--- s13-integracao-backend-bd/src/app.js
+++ s13-integracao-backend-bd/src/app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/dbClients", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db = mongoose.connection;
+db.on("error", console.log.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Sucessfully connected!");
+});
 
 const index = require("./routes/index");
 const clients = require("./routes/clients");
