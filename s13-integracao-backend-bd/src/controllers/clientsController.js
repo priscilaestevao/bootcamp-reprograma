d29f366ff72fd2642432fb2c79568a947e1663d1
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-07 23:50:06 -0300

    configuration of HTTP get and post methods

--- s13-integracao-backend-bd/src/controllers/clientsController.js
+++ s13-integracao-backend-bd/src/controllers/clientsController.js
@@ -1,12 +1,30 @@
 const clients = require("../models/clients");
 
-const getAllClients = (req, res) => {};
+const getAllClients = (req, res) => {
+  clients.find((err, clients) => {
+    if (err) {
+      res.status(424).send({ message: err.message });
+    }
+    res.status(200).send(clients);
+  });
+};
 
 const buyersClients = (req, res) => {};
 
 const clientByCPF = (req, res) => {};
 
-const newClient = (req, res) => {};
+const newClient = (req, res) => {
+  let client = new clients(req.body);
+  client.save((err) => {
+    if (err) {
+      res.status(424).send({ message: err.message });
+    }
+    res.status(201).send({
+      status: true,
+      menssage: "Client successfully included!",
+    });
+  });
+};
 
 module.exports = {
   getAllClients,
