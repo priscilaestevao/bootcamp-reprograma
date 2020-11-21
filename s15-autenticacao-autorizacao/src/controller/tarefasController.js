c176dba65bb6b65c2b1304ced60a1b73318f7de6
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-21 17:55:52 -0300

    tasks included with authenticated method

--- s15-autenticacao-autorizacao/src/controller/tarefasController.js
+++ s15-autenticacao-autorizacao/src/controller/tarefasController.js
@@ -2,13 +2,17 @@ const tasks = require("../models/tarefas");
 const SECRET = process.env.SECRET;
 const jwt = require("jsonwebtoken");
 
-const getAllTasks = (req, res) => {
+const auth = (req, res) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
     return res.status(401).send("Need to include headers.");
   }
-
   const token = authHeader.split(" ")[1];
+  return token;
+};
+
+const getAllTasks = (req, res) => {
+  const token = auth(req, res);
   jwt.verify(token, SECRET, (err) => {
     if (err) {
       return res.status(403).send({ message: err });
@@ -23,16 +27,22 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  let task = new tasks(req.body);
-  task.save((err) => {
+  const token = auth(req, res);
+  jwt.verify(token, SECRET, (err) => {
     if (err) {
-      res.status(424).send({ message: err });
+      return res.status(424).send({ message: err });
     }
-    res.status(200).send(task.toJSON());
-  })
-}
+    let task = new tasks(req.body);
+    task.save((err) => {
+      if (err) {
+        res.status(424).send({ message: err });
+      }
+      res.status(200).send(task.toJSON());
+    });
+  });
+};
 
 module.exports = {
   getAllTasks,
-  createTask
+  createTask,
 };
