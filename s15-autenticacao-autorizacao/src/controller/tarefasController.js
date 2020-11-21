ed383df933ba1e4a379f4a72a836f5f9f9303a3b
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-21 16:56:00 -0300

    authenticated route creation

--- s15-autenticacao-autorizacao/src/controller/tarefasController.js
+++ s15-autenticacao-autorizacao/src/controller/tarefasController.js
@@ -1,11 +1,24 @@
 const tasks = require("../models/tarefas");
+const SECRET = process.env.SECRET;
+const jwt = require("jsonwebtoken");
 
 const getAllTasks = (req, res) => {
-  tasks.find((err, tasks) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    return res.status(401).send("Need to include headers.");
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, SECRET, (err) => {
     if (err) {
-      return res.status(424).send({ message: err });
+      return res.status(403).send({ message: err });
     }
-    res.status(200).send(tasks);
+    tasks.find((err, tasks) => {
+      if (err) {
+        return res.status(424).send({ message: err });
+      }
+      res.status(200).send(tasks);
+    });
   });
 };
 
